e6ca671ba6d468c0431822bea5b12c5d736b0ba8
Michael Craun <michael.craun@gmail.com>
2021-07-31 13:31:21 -0400

    Initial Commit

---
+++ Shared/DiceCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceCalcApp.swift
+//  Shared
+//
+//  Created by Michael Craun on 7/31/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
